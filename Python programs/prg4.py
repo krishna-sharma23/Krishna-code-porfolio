6192c0249aac55563a9210686384a66b0be8dc01
krishna-sharma23 <ks3851040@gmail.com>
2026-01-29 11:18:09 +0530

    Delete Python programs directory

--- Python programs/prg4.py
+++
@@ -1,8 +0,0 @@
-#program to find out the largest digit in a number
-num=input("Enter the number")
-large=0
-for digit in num:
-        if large < int(digit):
-            large=digit
-print(f"the largest digit in the number is {large}")
-
--- Python programs/prg9.py
+++
@@ -1,6 +0,0 @@
-#Pattern1
-height=int(input("Enter the Height of the triangle"))
-for x in range(height):
-    for y in range(x+1):
-        print("*", end="")
-    print("")
